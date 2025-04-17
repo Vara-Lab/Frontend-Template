3ec091a70acf777429023680d8b6786045695ccc
A-mont <a.mont.24.777@gmail.com>
2025-04-17 13:41:38 -0600

    feat(Routes): Update routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Home } from "./home";
-import { Main } from "./Main";
+import { Main } from "./main";
 import { NotFound } from "./not-found";
 
 const routes = [
